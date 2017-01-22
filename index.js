b558c0197442c312bdc35c8b4131fb2f349d78cd
Daubert Jiang <jzh16s@hotmail.com>
2017-01-22 15:10:53 +0800

    Add Fortune Module

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app = express();
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-]
+var fortune = require('./lib/fortune');
 
 //set handlebars view-engine
 var handlebars = require('express-handlebars')
@@ -25,8 +19,7 @@ app.get('/',function(req,res){
 });
 
 app.get('/about',function(req,res){
-    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about',{fortune:randomFortune});
+    res.render('about',{fortune:fortune.getFortune()});
 });
 
 //404 catch-all Processor
