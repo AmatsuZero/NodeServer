a36767b500a2dc220aded82ba83dbc199a1febf2
Daubert Jiang <jzh16s@hotmail.com>
2017-01-23 09:28:47 +0800

    Before Ch08

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ var fortune = require('./lib/fortune');
 
 //set handlebars view-engine
 var handlebars = require('express-handlebars')
-    .create({defaultLayout:'main'});
+    .create({
+        defaultLayout:'main',
+        helpers:{
+            section:function (name,options) {
+                if(!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
+    });
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
+
+//disable 'x-powered-by' value in the response body
+app.disable('x-powered-by');
 
 //set static resources path
 app.use(express.static(__dirname + '/public'));
@@ -38,6 +50,52 @@ app.get('/tours/hood-river',function (req,res) {
 
 app.get('/tours/request-group-rate',function (req,res) {
     res.render('tours/request-group-rate');
+});
+
+app.get('/nursery-rhyme',function (req,res) {
+    res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function (req,res) {
+   res.json({
+       animal:'squirrel',
+       bodyPart:'tail',
+       adjective:'bushy',
+       noun:'heck'
+   })
+});
+
+function getWeatherData() {
+    return {
+        locations:[
+            {
+                name:'Portland',
+                forecastUrl:'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl:'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather:'Overcast',
+                temp:'54.1 F (12.3 C)'
+            },
+            {
+                name:'Bend',
+                forecastUrl:'http://www.wunderground.com/US/OR/Bend.html',
+                weather:'Partly Cloudy',
+                temp:'55.0 F (12.8 C)'
+            },
+            {
+                name:'Manzanita',
+                forecastUrl:'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl:'http://icons-ak.wxung.com/ic/c/k/rain.gif',
+                weather:'Light Rain',
+                temp:'55.0 F (12.8 C)'
+            }
+        ]
+    }
+}
+
+app.use(function (req,res,next) {
+   if(!res.locals.partials) res.locals.partials = {};
+   res.locals.partials.weather = getWeatherData();
+   next();
 });
 
 //404 catch-all Processor
